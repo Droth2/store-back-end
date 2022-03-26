bfbdd92550a82cf858c64a77e56e08a2ddb5f803
lord vader <dillon.roth11@gmail.com>
2022-03-25 23:32:40 -0700

    crud routes for products, categories, and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,14 +21,9 @@ router.get('/', (req, res) => {
         attributes: ['id', 'category_name']
       },
       {
-        model: ProductTag,
-        attributes: ['product_id', 'tag_id'],
-        include: [
-          {
-            model: Tag,
-            attributes: ['id', 'tag_name']
-          }
-        ]
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        as: 'tags'
       }
     ]
   })
@@ -60,14 +55,9 @@ router.get('/:id', (req, res) => {
         attributes: ['id', 'category_name']
       },
       {
-        model: ProductTag,
-        attributes: ['product_id', 'tag_id'],
-        include: [
-          {
-            model: Tag,
-            attributes: ['id', 'tag_name']
-          }
-        ]
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        as: 'tags'
       }
     ]
   })
